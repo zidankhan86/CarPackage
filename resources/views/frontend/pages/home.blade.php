1ff2aeea844b7d8d77152a74434af1402bef42d5
zidankhan86 <zidankhan718@gmail.com>
2023-12-12 23:30:38 +0600

    Fit:problem fixe issue

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.layout.app')
 
 @section('content')
-
+@include('frontend.components.fixed.hero')
 
 @include('frontend.components.about.home_about')
 
--- resources/views/frontend/pages/package.blade.php
+++ resources/views/frontend/pages/package.blade.php
@@ -2,6 +2,16 @@
 
 @section('content')
 
+<!-- Larger Header Section -->
+<header class="bg-dark py-12" style="background-image: url('{{ asset('https://www.linearity.io/blog/content/images/2023/06/how-to-create-a-car-NewBlogCover.png') }}'); background-size: cover; background-position: center;">
+    <div class="container px-4 px-lg-5 my-5">
+        <div class="text-center text-white">
+            <h1 class="display-1 fw-bolder">Discover Our Car Packages</h1>
+            <p class="lead fw-normal text-green-50 mb-6">Unleash the Joy of Driving with Our Premium Car Rental Services</p>
+            <a href="{{ route('home') }}" class="btn btn-success">Explore Now</a>
+        </div>
+    </div><br><br><br><br>
+</header>
 @include('frontend.components.product.product')
 
 
